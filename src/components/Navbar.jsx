9944b4ad9f63562f070af4ee4a8ffa4c14652587
mohammedafsalk <techafs456@gmail.com>
2023-11-21 19:48:42 +0530

    gradient effect for nav text

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Navbar() {
   return (
     <>
-      <div className="md:container md:mx-auto  h-16 text-4x flex justify-center items-center">
-        <span className="text-white  text-3xl">Image Slider</span>
+      <div className="md:container md:mx-auto  h-16 flex justify-center items-center">
+        <span className="  text-3xl font-semibold bg-gradient-to-r from-blue-500 via-yellow-500 to-green-500 bg-clip-text text-transparent inline-block ">Image Slider</span>
       </div>
     </>
   );
